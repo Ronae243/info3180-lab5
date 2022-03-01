23ec14c72b7d8bd06d361951888edba90efbf9f3
Ronae Johnson <ronae2004@gmail.com>
2022-03-01 04:18:16 -0500

    conected to db and created tables

--- app/config.py
+++ app/config.py
@@ -8,4 +8,4 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('DATABASE_URL')
